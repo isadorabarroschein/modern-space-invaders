6f2e012328cdc6bf5a8ca700371469ec87cd4f30
isadorabarroschein <isadorabc437@gmail.com>
2024-05-23 11:31:08 -0400

    add draw function in PowerUp

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -65,7 +65,13 @@ class Bomb {
         this.radius = 15;
     
     }
-    }
-    
 
-      
+draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.closePath();
+    c.fillStyle = "yellow"
+    c.fill();
+
+
+}
