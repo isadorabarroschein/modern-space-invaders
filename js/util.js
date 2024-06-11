0c19d3ddd951ed3f20e3ee94fa61b2836082754f
isadorabarroschein <isadorabc437@gmail.com>
2024-06-11 09:52:42 -0400

    add creatParticles function in util.js file

--- js/util.js
+++ js/util.js
@@ -22,6 +22,8 @@ onComplete: () => {
 });
 }
 
+
+
 function rectangularCollision({rectanglel, rectangle2}){
     return (
         rectanglel.position.y + rectnaglel.height >= rectangle2.position.y &&
@@ -30,4 +32,22 @@ function rectangularCollision({rectanglel, rectangle2}){
     );
 }
 
+function createPosition() {} 
+for(let i = 0; i < 15; i++) {
+    particle.push()
+        new Particle({
+            position: {
+                x: object.position.x + object.width / 2,
+                y: object.position.y + object.height / 2
+            },
+            velocity: {
+                x: (Math.random() - 0.5) * 2,
+                y: (Math.random() - 0.5) * 2
+            },
+            radius: Math.random() * 3,
+            color: color || "#BAA0DE",
+            fades
+            })
+        }
+    
     
