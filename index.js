6de2fbb0ecdc40daa55f0eda5e444a8025dde478
isadorabarroschein <isadorabc437@gmail.com>
2024-06-13 07:54:18 -0400

    add scoreEl

--- index.js
+++ index.js
@@ -1,6 +1,95 @@
 const scoreEl = document.querySelector("#scoreEl");
 const  canvas = document.querySelector("#canvas");
-canvas c = canvas.getContext("2d");
+canvas  = canvas.getContext("2d");
 
 canvas.whidth = 1024;
 canvas.height = 576;
+
+let player = new Player();
+let projectiles = [];
+let grids = [];
+let invaderProjectiles = [];
+let particles = [];
+let bombs = [];
+let powerUps = []; 
+ 
+let keys = {
+    Arroeleft: {
+        pressed: false
+    },
+    Arrowright: {
+        pressed: false
+    },
+      space: {
+        pressed: false
+    }
+};
+
+let frames = 0;
+let randomInterval = Math.floor(Math.random() * 500 + 500);
+
+let game = {
+    over: false,
+    active: true
+};
+
+let score = 0;
+
+let spanBuffer = 500;
+let fps =60;
+let fpsInterval = 100 / fps;
+
+let msPrev = window.perfumace.now();
+
+function init() {
+  player = new Player();
+ projectiles = [];
+ grids = [];
+ invaderProjectiles = [];
+ particles = [];
+ bombs = [];
+ powerUps = []; 
+ 
+ keys = {
+    Arroeleft: {
+        pressed: false
+    },
+    Arrowright: {
+        pressed: false
+    },
+      space: {
+        pressed: false
+    }
+};
+
+ frames = 0;
+ randomInterval = Math.floor(Math.random() * 500 + 500);
+
+ game = {
+    over: false,
+    active: true
+};
+
+ score = 0;
+
+for (let i  = 0; i < 100; i ++) {
+particles.push(
+    new Particle(
+        position: {
+            x: Math.floor(Math.random() * canvas.width),
+            y: Math.floor(Math.random() * canvas.height)
+        },
+        velocity: {
+           x:0
+           y: 0.3
+        },
+        radius: Math.random() * 2,
+        color: "white"
+    
+)
+);
+}
+}
+
+function enGame() {
+    Audio.gameOver
