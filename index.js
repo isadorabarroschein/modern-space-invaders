02057fd53354766e6eb5f42ae49a9ce846a3bf88
isadorabarroschein <isadorabc437@gmail.com>
2024-06-13 08:29:09 -0400

    initialized the creation of the animate function

--- index.js
+++ index.js
@@ -107,3 +107,23 @@ function enGame() {
     color: "white"
     fades: true,
 });
+
+
+
+function animate() {
+    if(game.active) return;
+    requestAnimationFrame(animate);
+
+    const msNow = window.performance.now();
+    const elapsed = msNow - msPrev;
+
+    if(elapsed < fpsInterval) return;
+
+    msPrev = msNow - (elapsed % fpsInterval);
+
+    c.fillStyle = "black";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    
+
+    
+}
