eb324800595a07b23b57da9795e45680c278da4d
isadorabarroschein <isadorabc437@gmail.com>
2024-06-20 09:45:28 -0400

    fixed

--- js/util.js
+++ js/util.js
@@ -1,53 +1,52 @@
-function randomBetween(min, max){
-    return Math.random() * (max - min)+ m
+function randomBetween(min, max) {
+  return Math.random() * (max - min) + min;
 }
-function creaateScoreLabel( { score = 100, object} ) {
-    const scoreLabel = document.createElement("label");
-    scoreLabel.innerHTML = score;
-    scoreLabel.style.poition = "absolute";
-    scoreLabel.style.color = "white";
-    scoreLabel.style.left = object.position. x+ "px";    
-    scoreLabel.style.top = object.position.y + "px";
-   scoreLabel.style.Userselect = "nome";
 
-   document.querySelector("#aparentDiv").appendChild(scoreLabel);
+function createScoreLabel({ score = 100, object }) {
+  const scoreLabel = document.createElement("label");
+  scoreLabel.innerHTML = score;
+  scoreLabel.style.position = "absolute";
+  scoreLabel.style.color = "white";
+  scoreLabel.style.top = object.position.y + "px";
+  scoreLabel.style.left = object.position.x + "px";
+  scoreLabel.style.userSelect = "none";
 
-   gsap.to(scoreLabel, {
-    opacity: 0 ,
+  document.querySelector("#parentDiv").appendChild(scoreLabel);
+
+  gsap.to(scoreLabel, {
+    opacity: 0,
     y: -30,
-    duraction: 0,75 , 
-onComplete: () => {
-    document.querySelector("#aparentDiv").removeChild(scoreLabel);
+    duration: 0.75,
+    onComplete: () => {
+      document.querySelector("#parentDiv").removeChild(scoreLabel);
+    }
+  });
 }
-});
+
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.position.y + rectangle1.height >= rectangle2.position.y &&
+    rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+    rectangle1.position.x <= rectangle2.position.x + rectangle2.width
+  );
 }
 
-
-
-function rectangularCollision({rectanglel, rectangle2}){
-    return (
-        rectanglel.position.y + rectnaglel.height >= rectangle2.position.y &&
-        rectangle2.position.x + rectanglel.whidth >= rectangle2.position.x &&
-        rectanglel.position.x + rectanglel.position + rectangle2.whidth
+function createParticles({ object, color, fades }) {
+  for (let i = 0; i < 15; i++) {
+    particles.push(
+      new Particle({
+        position: {
+          x: object.position.x + object.width / 2,
+          y: object.position.y + object.height / 2
+        },
+        velocity: {
+          x: (Math.random() - 0.5) * 2,
+          y: (Math.random() - 0.5) * 2
+        },
+        radius: Math.random() * 3,
+        color: color || "#BAA0DE",
+        fades
+      })
     );
+  }
 }
-
-function createPosition() {} 
-for(let i = 0; i < 15; i++) {
-    particle.push()
-        new Particle({
-            position: {
-                x: object.position.x + object.width / 2,
-                y: object.position.y + object.height / 2
-            },
-            velocity: {
-                x: (Math.random() - 0.5) * 2,
-                y: (Math.random() - 0.5) * 2
-            },
-            radius: Math.random() * 3,
-            color: color || "#BAA0DE",
-            fades
-            })
-        }
-    
-    
